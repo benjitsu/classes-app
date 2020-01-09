2c6a6a43d00b8b2406b40a947bd85027b386fbfe
Ben <benjitsu@users.noreply.github.com>
2020-01-08 18:01:27 -0600

    viarables moved into ifthen

--- scripts.js
+++ scripts.js
@@ -1,54 +1,3 @@
-var bjj = [
-  { day: 'Monday', title: 'All Levels', time: '12:00 - 1:00pm' },
-  { day: 'Monday', title: 'All Levels', time: '6:30 - 8:00pm' },
-  { day: 'Monday', title: 'Beginners', time: '8:00 - 9:00pm' },
-  { day: 'Tuesday', title: 'All Levels', time: '07:00 - 8:00am' },
-  { day: 'Tuesday', title: 'Competition Training', time: '12:00 - 1:00pm' },
-  { day: 'Tuesday', title: 'Beginners', time: '5:00 - 6:30pm' },
-  { day: 'Wednesday', title: 'All Levels', time: '12:00 - 1:00pm' },
-  { day: 'Wednesday', title: 'All Levels', time: '6:30 - 8:00pm' },
-  { day: 'Wednesday', title: 'Beginners', time: '8:00 - 9:00pm' },
-  { day: 'Thursday', title: 'All Levels', time: '07:00 - 8:00am' },
-  { day: 'Thursday', title: 'Competition Training', time: '12:00 - 1:00pm' },
-  { day: 'Thursday', title: 'Beginners', time: '5:00 - 6:30pm' },
-  { day: 'Friday', title: 'All Levels', time: '12:00 - 1:00pm' },
-  { day: 'Friday', title: 'Beginners', time: '5:30 - 6:30pm' },
-  { day: 'Saturday', title: 'All Levels', time: '11:30am - 1:00pm' },
-  { day: 'Sunday', title: 'Open Mat', time: '10:00am - 12:00pm' }
-]
-
-var judo = [
-  { day: 'Monday', title: '& BJJ Takedowns', time: '5:30 - 6:30pm' },
-  { day: 'Tuesday', title: 'Adults & kids', time: '8:00 - 9:30pm' },
-  { day: 'Wednesday', title: '& BJJ Takedowns', time: '5:30 - 6:30pm' },
-  { day: 'Thursday', title: 'Adults & kids', time: '8:00 - 9:30pm' }
-]
-
-var noGi = [
-   { day: 'Thursday', title: 'No Gi Grapplling', time: '6:30 - 8:00pm' },
-   { day: 'Saturday', title: 'No Gi Grapplling', time: '10:30 - 11:30am' }
-]
-
-var kidsBjj = [
-  { day: 'Monday', title: 'Ages 5-7', time: '3:45 - 4:30pm' },
-  { day: 'Monday', title: 'Ages 8-14', time: '4:30 - 5:30pm' },
-  { day: 'Tuesday', title: 'Ages 8-14', time: '4:30 - 5:30pm' },
-  { day: 'Wednesday', title: 'Ages 5-7', time: '3:45 - 4:30pm' },
-  { day: 'Wednesday', title: 'Ages 8-14', time: '4:30 - 5:30pm' },
-  { day: 'Thursday', title: 'Ages 8-14', time: '4:30 - 5:30pm' },
-  { day: 'Friday', title: 'Ages 5-7', time: '3:45 - 4:30pm' },
-  { day: 'Friday', title: 'Ages 8-14', time: '4:30 - 5:30pm' }
-]
-
-var wrestling = [
-  { day: 'Tuesday', title: 'Wrestling', time: '6:30 - 8:30pm' }
-]
-
-var mobilityTraining = [
-    { day: 'Tuesday', title: 'Mobility Training', time: '11:00am - 12:00pm' },
-    { day: 'Thursday', title: 'Mobility Training', time: '11:00am - 12:00pm' }
-]
-
 var today = '';
 
 // turn date number to day of week
@@ -89,20 +38,65 @@ function showClasses(classArray) {
 //append html to sidebar 
 if (document.querySelector('.brazilian-jiu-jitsu .sideBar') !== null) {
   var sideBar = document.querySelector('.brazilian-jiu-jitsu .sideBar');
+  var bjj = [
+    { day: 'Monday', title: 'All Levels', time: '12:00 - 1:00pm' },
+    { day: 'Monday', title: 'All Levels', time: '6:30 - 8:00pm' },
+    { day: 'Monday', title: 'Beginners', time: '8:00 - 9:00pm' },
+    { day: 'Tuesday', title: 'All Levels', time: '07:00 - 8:00am' },
+    { day: 'Tuesday', title: 'Competition Training', time: '12:00 - 1:00pm' },
+    { day: 'Tuesday', title: 'Beginners', time: '5:00 - 6:30pm' },
+    { day: 'Wednesday', title: 'All Levels', time: '12:00 - 1:00pm' },
+    { day: 'Wednesday', title: 'All Levels', time: '6:30 - 8:00pm' },
+    { day: 'Wednesday', title: 'Beginners', time: '8:00 - 9:00pm' },
+    { day: 'Thursday', title: 'All Levels', time: '07:00 - 8:00am' },
+    { day: 'Thursday', title: 'Competition Training', time: '12:00 - 1:00pm' },
+    { day: 'Thursday', title: 'Beginners', time: '5:00 - 6:30pm' },
+    { day: 'Friday', title: 'All Levels', time: '12:00 - 1:00pm' },
+    { day: 'Friday', title: 'Beginners', time: '5:30 - 6:30pm' },
+    { day: 'Saturday', title: 'All Levels', time: '11:30am - 1:00pm' },
+    { day: 'Sunday', title: 'Open Mat', time: '10:00am - 12:00pm' }
+  ]
   sideBar.innerHTML = showClasses(bjj);
 } else if (document.querySelector('.judo .sideBar') !== null) {
   var sideBar = document.querySelector('.judo .sideBar');
+var judo = [
+  { day: 'Monday', title: '& BJJ Takedowns', time: '5:30 - 6:30pm' },
+  { day: 'Tuesday', title: 'Adults & kids', time: '8:00 - 9:30pm' },
+  { day: 'Wednesday', title: '& BJJ Takedowns', time: '5:30 - 6:30pm' },
+  { day: 'Thursday', title: 'Adults & kids', time: '8:00 - 9:30pm' }
+]
   sideBar.innerHTML = showClasses(judo);
 } else if (document.querySelector('.no-gi-grappling .sideBar') !== null) {
   var sideBar = document.querySelector('.no-gi-grappling .sideBar');
+  var noGi = [
+    { day: 'Thursday', title: 'No Gi Grapplling', time: '6:30 - 8:00pm' },
+    { day: 'Saturday', title: 'No Gi Grapplling', time: '10:30 - 11:30am' }
+ ]
   sideBar.innerHTML = showClasses(noGi);
 } else if (document.querySelector('.kids-jiu-jitsu .sideBar') !== null) {
   var sideBar = document.querySelector('.kids-jiu-jitsu .sideBar');
+  var kidsBjj = [
+    { day: 'Monday', title: 'Ages 5-7', time: '3:45 - 4:30pm' },
+    { day: 'Monday', title: 'Ages 8-14', time: '4:30 - 5:30pm' },
+    { day: 'Tuesday', title: 'Ages 8-14', time: '4:30 - 5:30pm' },
+    { day: 'Wednesday', title: 'Ages 5-7', time: '3:45 - 4:30pm' },
+    { day: 'Wednesday', title: 'Ages 8-14', time: '4:30 - 5:30pm' },
+    { day: 'Thursday', title: 'Ages 8-14', time: '4:30 - 5:30pm' },
+    { day: 'Friday', title: 'Ages 5-7', time: '3:45 - 4:30pm' },
+    { day: 'Friday', title: 'Ages 8-14', time: '4:30 - 5:30pm' }
+  ]
   sideBar.innerHTML = showClasses(kidsBjj);
 } else if (document.querySelector('.wrestling .sideBar') !== null) {
   var sideBar = document.querySelector('.wrestling .sideBar');
+  var wrestling = [
+    { day: 'Tuesday', title: 'Wrestling', time: '6:30 - 8:30pm' }
+  ]
   sideBar.innerHTML = showClasses(wrestling);
 } else if (document.querySelector('.mobility-training .sideBar') !== null) {
   var sideBar = document.querySelector('.mobility-training .sideBar');
+  var mobilityTraining = [
+    { day: 'Tuesday', title: 'Mobility Training', time: '11:00am - 12:00pm' },
+    { day: 'Thursday', title: 'Mobility Training', time: '11:00am - 12:00pm' }
+]
   sideBar.innerHTML = showClasses(mobilityTraining);
 }
